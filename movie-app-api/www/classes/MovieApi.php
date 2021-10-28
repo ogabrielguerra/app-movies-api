db21d9318e019857e385c3d4793e298d3de43904
Gabriel Guerra <gabrielguerra@gmail.com>
2021-10-28 16:18:00 -0300

    Adding CORS control feature

--- movie-app-api/www/classes/MovieApi.php
+++ movie-app-api/www/classes/MovieApi.php
@@ -10,7 +10,7 @@ class MovieApi
         $limit = $this->numPages + 1;
 
         for($i=1; $i<$limit; $i++){
-//            $url = 'https://api.themoviedb.org/3/movie/upcoming?api_key=1f54bd990f1cdfb230adb312546d765d&page='.$i;
+            $url = 'https://api.themoviedb.org/3/movie/upcoming?api_key=1f54bd990f1cdfb230adb312546d765d&page='.$i;
             $json = json_decode(file_get_contents($url));
 
             foreach ($json->results as $movie){
--- movie-app-api/www/index.php
+++ movie-app-api/www/index.php
@@ -5,6 +5,22 @@
     use www\classes\Genre;
     use www\classes\CacheBuilder;
 
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+// Access-Control headers are received during OPTIONS requests
+// if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+//     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+//         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
+
+//     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+//         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+// }
+
     function home(){
         return '
         <div style="text-align: center; margin: 40% 0px 0px 0px">
